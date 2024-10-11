648cc8899550a8f49fcd9a46b8a6c35080a4a7e1
Md. Mehedi Hasan <126223424+huzzathasan@users.noreply.github.com>
2024-10-11 22:23:53 +0600

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,21 @@
 import MasonryGallery from "../components/MasonryGallery";
 import { Link } from "react-router-dom";
+import { pb } from "../pb.config";
 
 const Home = () => {
+  const authenticated = pb.authStore.isValid;
+ 
   return (
     <div className="min-h-screen bg-gray-100 relative">
-<div className="bg-blue-500 p-2 rounded-md text-white absolute top-2 right-2 z-50"><Link to="/app-admin-login" >Login</Link></div>
+<div className="bg-blue-500 p-1 rounded-md text-white absolute top-2 right-2 z-50">
+{
+authenticated
+?
+<Link to="/app-admin" >Add</Link>
+:
+<Link to="/app-admin-login" >Login</Link>
+}
+</div>
       <h1 className="text-center text-3xl font-bold py-8">Media Gallery</h1>
       <MasonryGallery />
     </div>
